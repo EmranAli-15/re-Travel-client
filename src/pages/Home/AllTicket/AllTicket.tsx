442dfb851a20bc289935bbf1e1fde141a3f05832
EmranAli-15 <mohammademranali15@gmail.com>
2023-07-15 16:48:38 +0600

    all ticket section almost done

--- src/pages/Home/AllTicket/AllTicket.tsx
+++ src/pages/Home/AllTicket/AllTicket.tsx
@@ -3,7 +3,13 @@ import { useEffect, useState } from "react";
 
 const AllTicket = () => {
         type ticketData = {
-                img: string
+                img: string,
+                from: string,
+                to: string,
+                price: number,
+                agency: string,
+                logo: string,
+                service: string
         }
 
         const [allTicket, setAllTicket] = useState([]);
@@ -14,11 +20,29 @@ const AllTicket = () => {
                         .then(data => setAllTicket(data))
         }, [])
         return (
-                <div>
+                <div className="grid grid-cols-2 gap-2 mt-20">
                         {
-                                allTicket.map((ticket: ticketData, index) => <div key={index}>
-                                        <img src={ticket.img} alt="" />
-                                </div>)
+                                allTicket.map((ticket: ticketData, index) =>
+                                        <div key={index}>
+                                                <div className="gap-x-1 md:gap-x-4 md:flex items-center p-1 border-gray-400 rounded-lg border-[1px]">
+                                                        <div className="md:w-[40%]">
+                                                                <img src={ticket.img} className="w-full md:w-64 rounded-lg" />
+                                                        </div>
+
+                                                        <div className="md:w-[60%]">
+                                                                <div>
+                                                                        <p>From : {ticket.from}</p>
+                                                                        <p>To : {ticket.to}</p>
+                                                                        <p>Price : {ticket.price}</p>
+                                                                        <p>Service : {ticket.service}</p>
+                                                                </div>
+                                                                <div className="md:flex justify-between items-center">
+                                                                        <p>Agency: {ticket.agency}</p>
+                                                                        <button className="btn mt-4 md:mt-0 btn-xs btn-outline btn-gray-400">cart</button>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </div>)
                         }
                 </div>
         );
